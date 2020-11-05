cc8532729ecabb02baeac06e4cafffc0324864fd
wangerfan <ivan_sir@163.com>
2020-11-06 01:03:04 +0800

    注解解析

---
+++ src/cn/wangerfan/annotation/Demo1.java
@@ -0,0 +1,7 @@
+package cn.wangerfan.annotation;
+
+public class Demo1 {
+    public void show1(){
+        System.out.println("show1执行");
+    }
+}
--- src/cn/wangerfan/annotation/Test.java
+++ src/cn/wangerfan/annotation/Test.java
@@ -1,4 +1,20 @@
 package cn.wangerfan.annotation;
 
+/**
+ * 注解解析
+ */
+@Pro(className = "cn.wangerfan.annotation.Demo1",methodName = "show1")
 public class Test {
+
+    public static void main(String[] args) {
+        //获取该类的字节码文件
+        Class<Test> testClass = Test.class;
+        //获取注解的字节码对象
+        Pro an = testClass.getAnnotation(Pro.class);
+        //获取抽象方法
+        String s = an.className();
+        String s1 = an.methodName();
+        System.out.println(s+"--------"+s1);
+    }
+
 }
